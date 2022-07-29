c7f351dd19d34ed80ba42947ba076538418147f3
Albertino Padin <albertino.padin@gmail.com>
2022-07-29 02:14:15 -0700

    Binding buffers in loop.

--- test-cpp/cherno.cpp
+++ test-cpp/cherno.cpp
@@ -199,6 +199,11 @@ int main(void)
     ASSERT(location != -1);
     GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
 
+    // Temporarily unbind everything:
+    GLCall(glUseProgram(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
     float r = 0.0f;
     float increment = 0.05f;
     /* Loop until the user closes the window */
@@ -207,7 +212,20 @@ int main(void)
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        // Rebind:
+        GLCall(glUseProgram(shader));
+
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));  // Uniform is set per draw call
+
+        // Rebind:
+        GLCall(glBindBuffer(GL_ARRAY_BUFFER, VBO));
+
+        // Specifying layout of buffer
+        GLCall(glEnableVertexAttribArray(0));
+        GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+
+        // Rebind:
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
 
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
