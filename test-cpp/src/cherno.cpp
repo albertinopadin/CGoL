7ccb9898ea3c08e52a5e6acff84d05d43a504c07
Albertino Padin <albertino.padin@gmail.com>
2022-08-07 23:47:41 -0700

    Rendering multiple objects by binding new projection matrix.

--- test-cpp/src/cherno.cpp
+++ test-cpp/src/cherno.cpp
@@ -64,10 +64,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     float positions[] = {
-        100.0f, 100.0f, 0.0f, 0.0f,
-        200.0f, 100.0f, 1.0f, 0.0f,
-        200.0f, 200.0f, 1.0f, 1.0f,
-        100.0f, 200.0f, 0.0f, 1.0f
+        -50.0f, -50.0f, 0.0f, 0.0f,
+         50.0f, -50.0f, 1.0f, 0.0f,
+         50.0f,  50.0f, 1.0f, 1.0f,
+        -50.0f,  50.0f, 0.0f, 1.0f
     };
 
     unsigned int indices[] = {
@@ -91,7 +91,7 @@ int main(void)
 
     // Orthographic projection
     glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
     // glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
 
     // glm::mat4 mvp = proj * view * model;
@@ -102,8 +102,6 @@ int main(void)
     Shader shader("../resources/shaders/Basic.shader");
     shader.Bind();
     shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-    // shader.SetUniformMat4f("u_MVP", proj);
-    // shader.SetUniformMat4f("u_MVP", mvp);
 
     Texture texture("../resources/textures/ChernoLogoAlpha.png");
     texture.Bind();
@@ -121,7 +119,8 @@ int main(void)
     ImGui_ImplGlfwGL3_Init(window, true);
     ImGui::StyleColorsDark();
 
-    glm::vec3 translation(200, 200, 0);
+    glm::vec3 translationA(200, 200, 0);
+    glm::vec3 translationB(400, 200, 0);
 
     float r = 0.0f;
     float increment = 0.05f;
@@ -132,16 +131,22 @@ int main(void)
         renderer.Clear();
 
         ImGui_ImplGlfwGL3_NewFrame();
-
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-        glm::mat4 mvp = proj * view * model;
-
-        // Rebind:
-        shader.Bind();
-        shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);  // Uniform is set per draw call
-        shader.SetUniformMat4f("u_MVP", mvp);
         
-        renderer.Draw(va, ib, shader);
+        {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+            glm::mat4 mvp = proj * view * model;
+            shader.Bind();
+            shader.SetUniformMat4f("u_MVP", mvp);
+            renderer.Draw(va, ib, shader);
+        }
+        
+        {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+            glm::mat4 mvp = proj * view * model;
+            shader.Bind();
+            shader.SetUniformMat4f("u_MVP", mvp);
+            renderer.Draw(va, ib, shader);
+        }
 
         if (r > 1.0f)
             increment = -0.05f;
@@ -151,7 +156,8 @@ int main(void)
         r += increment;
 
         {
-            ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+            ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+            ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
         }
 
