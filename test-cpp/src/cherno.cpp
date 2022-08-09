d3d8e1a0538262bb2688fb1e553f6f74de3c2a87
Albertino Padin <albertino.padin@gmail.com>
2022-08-09 00:56:40 -0700

    Implementing test framework.

--- test-cpp/src/cherno.cpp
+++ test-cpp/src/cherno.cpp
@@ -12,6 +12,7 @@
 #include "Texture.h"
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw_gl3.h"
+#include "tests/TestClearColor.h"
 
 /*  
     From The Cherno's series on OpenGL
@@ -55,7 +56,7 @@ int main(void)
 
     glfwSwapInterval(1);  // Make refresh rate same as monitor
 
-    // // // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
     glewExperimental = GL_TRUE;
     // Initialize GLEW to setup the OpenGL Function pointers
     glewInit();
@@ -63,55 +64,8 @@ int main(void)
     std::cout << "OpenGL version: " << std::endl;
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[] = {
-        -50.0f, -50.0f, 0.0f, 0.0f,
-         50.0f, -50.0f, 1.0f, 0.0f,
-         50.0f,  50.0f, 1.0f, 1.0f,
-        -50.0f,  50.0f, 0.0f, 1.0f
-    };
-
-    unsigned int indices[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-
     GLCall(glEnable(GL_BLEND));                                 // Enable blending
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));  // Setting blending function
-
-    VertexArray va;
-    VertexBuffer vb(positions, 4 * 4 * sizeof(float));  // 2 floats of positions, 2 of texture coords
-
-    VertexBufferLayout layout;
-    layout.Push<float>(2);
-    layout.Push<float>(2);  // Texture coords
-    va.AddBuffer(vb, layout);
-
-    // Setting up index buffer
-    IndexBuffer ib(indices, 6);
-
-    // Orthographic projection
-    glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-    // glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-
-    // glm::mat4 mvp = proj * view * model;
-
-    // glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
-    // glm::vec4 result = proj * vp;  // Tried setting breakpoint here in vscode...
-
-    Shader shader("../resources/shaders/Basic.shader");
-    shader.Bind();
-    shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-    Texture texture("../resources/textures/ChernoLogoAlpha.png");
-    texture.Bind();
-    shader.SetUniform1i("u_Texture", 0);  // Setting texture slot
-
-    // Temporarily unbind everything:
-    va.Unbind();
-    vb.Unbind();
-    ib.Unbind();
-    shader.Unbind();
 
     Renderer renderer;
 
@@ -119,48 +73,21 @@ int main(void)
     ImGui_ImplGlfwGL3_Init(window, true);
     ImGui::StyleColorsDark();
 
-    glm::vec3 translationA(200, 200, 0);
-    glm::vec3 translationB(400, 200, 0);
+    test::TestClearColor test;
 
-    float r = 0.0f;
-    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         renderer.Clear();
 
+        test.OnUpdate(0.0f);
+        test.OnRender();
+
         ImGui_ImplGlfwGL3_NewFrame();
+
+        test.OnImGuiRender();
         
-        {
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-            glm::mat4 mvp = proj * view * model;
-            shader.Bind();
-            shader.SetUniformMat4f("u_MVP", mvp);
-            renderer.Draw(va, ib, shader);
-        }
-        
-        {
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-            glm::mat4 mvp = proj * view * model;
-            shader.Bind();
-            shader.SetUniformMat4f("u_MVP", mvp);
-            renderer.Draw(va, ib, shader);
-        }
-
-        if (r > 1.0f)
-            increment = -0.05f;
-        else if (r < 0.0f)
-            increment = 0.05f;
-
-        r += increment;
-
-        {
-            ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-            ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-        }
-
         ImGui::Render();
         ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
